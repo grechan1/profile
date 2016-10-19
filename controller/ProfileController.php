b1ecdc6b95104998f744c78cc7b72e5fa07cde77
grechan1 <grechannn@mail.ru>
2016-10-19 16:37:35 +0300

    Add files via upload

--- controller/ProfileController.php
+++ controller/ProfileController.php
@@ -1,1 +1,140 @@
+<?php
 
+namespace backend\modules\profile\controllers;
+
+use Yii;
+use backend\modules\profile\models\Profile;
+use backend\modules\profile\models\ProfileSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * ProfileController implements the CRUD actions for Profile model.
+ */
+class ProfileController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Profile models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $profile=new Profile();
+        if (Yii::$app->request->isPjax && $profile->load(Yii::$app->request->post()) && $profile->save(true, $profile->attributes()))
+        {
+            //$profile->load(Yii::$app->request->post());
+            //$user_id=Yii::$app->request->post('user_id');
+            //$model=$this->findModel($user_id);
+            //$model->balance=$profile->balance;
+            //$model->save($runValidation = true, $model->attributes());
+            //$profile->isNewRecord=false;
+            //if ($profile->save($runValidation = true, $profile->attributes()))
+            //if (Yii::$app->request->isPjax && $profile->load(Yii::$app->request->post()) && $profile->save($runValidation = true, $profile->attributes()))
+
+            $profile = new Profile();
+        }
+
+        $searchModel = new ProfileSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'profile'=>$profile
+        ]);
+    }
+
+    /**
+     * Displays a single Profile model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Profile model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Profile();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->user_id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Profile model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->user_id==$id && $model->save(true, $model->attributes())) {
+             return $this->redirect(['view', 'id' => $id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Profile model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Profile model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Profile the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Profile::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
---
+++ controller/ProfileFieldController.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace backend\modules\profile\controllers;
+
+use Yii;
+use backend\modules\profile\models\ProfileField;
+use backend\modules\profile\models\Profile;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\helpers\Url;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\db\Exception;
+
+/**
+ * ProfileFieldController implements the CRUD actions for ProfileField model.
+ */
+class ProfileFieldController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+
+    private static $_widgets = array();
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all ProfileField models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => ProfileField::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single ProfileField model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+    /**
+     * Creates a new ProfileField model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new ProfileField();
+        $profile=new Profile();
+        //$fieldTypeList=ArrayHelper::map($model->profile_filed_type, 'id', 'name');
+        //$scheme=Yii::$app->db->schema;
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            //$model->field_type=$this->fieldType($model->field_type);
+            $sql = 'ALTER TABLE '.$profile->tableName().' ADD `'.$model->varname.'` ';
+            $sql .= $model->field_type;
+            if (
+                $model->field_type!='TEXT'
+                && $model->field_type!='DATE'
+                && $model->field_type!='BOOL'
+                && $model->field_type!='BLOB'
+                && $model->field_type!='BINARY'
+            )
+                $sql .= '('.$model->field_size.')';
+            $sql .= ' NOT NULL ';
+
+            //if ($model->field_type!='TEXT'&&$model->field_type!='BLOB'||$scheme!='CMysqlSchema') {
+            if ($model->field_type!='TEXT'&&$model->field_type!='BLOB') {
+                if ($model->default)
+                    $sql .= " DEFAULT '".$model->default."'";
+                else
+                    $sql .= ((
+                        $model->field_type=='TEXT'
+                            ||$model->field_type=='VARCHAR'
+                            ||$model->field_type=='BLOB'
+                            ||$model->field_type=='BINARY'
+                    )?" ":(($model->field_type=='DATE')?" DEFAULT '0000-00-00'":" DEFAULT 0"));
+            }
+            /*return $this->render('create', [
+                'model' => $model,
+                'sql'=>$sql
+            ]);*/
+            $transaction = Yii::$app->db->beginTransaction();
+            try {
+                if (Yii::$app->db->createCommand($sql)->execute()==0)
+                    throw new Exception('Command cannot be executed.');
+                if (!$model->save())
+                    throw new Exception('Model cannot be saved.');
+                $transaction->commit();
+                $this->redirect(array('view','id'=>$model->id));
+            } catch (Exception $e) {
+                $transaction->rollBack();
+                throw new Exception ("catch transaction, ".$e->getMessage());
+            }
+        } else {
+            //$this->registerScript();
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing ProfileField model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing ProfileField model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $model=$this->findModel($id);
+        $profile=new Profile();
+        $sql = 'ALTER TABLE '.$profile->tableName().' DROP `'.$model->varname.'` ';
+        try {
+            if (Yii::$app->db->createCommand($sql)->execute()==0)
+                throw new Exception('Command cannot be executed.');
+            if (!$model->delete())
+                throw new Exception('Model cannot be saved.');
+            return $this->redirect(['index']);
+        } catch (Exception $e) {
+            throw new Exception ("catch Exception, ".$e->getMessage());
+        }
+    }
+
+    /**
+     * Finds the ProfileField model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return ProfileField the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = ProfileField::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+    public function fieldType($type) {
+        $profile_field=new ProfileField;
+        $field_type_arr = $profile_field->profile_filed_type;
+        $type=$field_type_arr[$type];
+        return $type;
+    }
+
+    public static function getWidgets($fieldType='') {
+        $basePath=Yii::getAlias('app.modules.profile.components');
+        $widgets = [];
+        $list = [''=>'No'];
+        if (self::$_widgets) {
+            $widgets = self::$_widgets;
+        } else {
+            $d = dir($basePath);
+            while (false !== ($file = $d->read())) {
+                if (strpos($file,'UW')===0) {
+                    list($className) = explode('.',$file);
+                    if (class_exists($className)) {
+                        $widgetClass = new $className;
+                        if ($widgetClass->init()) {
+                            $widgets[$className] = $widgetClass->init();
+                            if ($fieldType) {
+                                if (in_array($fieldType,$widgets[$className]['fieldType'])) $list[$className] = $widgets[$className]['label'];
+                            } else {
+                                $list[$className] = $widgets[$className]['label'];
+                            }
+                        }
+                    }
+                }
+            }
+            $d->close();
+        }
+        return array($list,$widgets);
+    }
+}
