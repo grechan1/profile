518673ed45f4a1786c9f4c372232810cfa4848a9
grechan1 <grechannn@mail.ru>
2016-10-19 16:10:53 +0300

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace backend\modules\profile;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'backend\modules\profile\controllers';
+    public $defaultRoute = 'profile';
+    public $controllerMap = [
+        'profilefield'    => 'backend\modules\profile\controllers\ProfileFieldController',
+    ];
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+        $this->setAliases([
+            '@profile-assets' => __DIR__ . '/assets'
+        ]);
+    }
+}
